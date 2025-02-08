2d7a5fe5d21f325a0b4a3e3a21eafe65a0b5cb18
lorenzolpandolfo <lorenzopandolfo2004@gmail.com>
2025-02-08 16:24:00 -0300

    invert status

--- tin-store/src/App.jsx
+++ tin-store/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
 
   return <>
     <SearchBar />
-    {!status && <InstallModal packageName={packageName} />}
+    {status && <InstallModal packageName={packageName} />}
   </>;
 };
 
